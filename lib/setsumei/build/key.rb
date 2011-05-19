1b725606ebb9065753f61cdd939bcf2d8d51ef4e
Jon Rowe <mail@jonrowe.co.uk>
2011-05-19 10:16:49 +0100

    key combination detection

--- lib/setsumei/build/key.rb
+++ lib/setsumei/build/key.rb
@@ -1,7 +1,28 @@
 module Setsumei
   module Build
     class Key
+      def Key.for(name,options = { given: [name] } )
+        possible_keys = options[:given]
+        lower_camel_case(name,possible_keys) || at_symbol_case(name,possible_keys) || upper_camel_case(name,possible_keys)
+      end
+      def Key.lower_camel_case name, keys = nil
+        lower_camel_case = name.to_s.gsub(/_[a-zA-Z]/) { |a| a[1].upcase }
+        return_if lower_camel_case, in: keys
+      end
+      def Key.at_symbol_case name, keys = nil
+        at_symbol_case = "@" + lower_camel_case(name)
+        return_if at_symbol_case, in: keys
+      end
+      def Key.upper_camel_case name, keys = nil
+        lower_camel_case = self.lower_camel_case(name)
+        upper_camel_case = lower_camel_case[0].upcase + lower_camel_case[1..-1]
+        return_if upper_camel_case, in: keys
+      end
 
+      private
+        def self.return_if value, options = { in: [] }
+          (options[:in] || [value]).select { |possible| possible == value }.first
+        end
     end
   end
 end
--- spec/setsumei/build/key_spec.rb
+++ spec/setsumei/build/key_spec.rb
@@ -3,7 +3,86 @@ require 'spec_helper'
 module Setsumei
   module Build
     describe Key do
+      describe ".for name, given: hash_keys" do
+        let(:hash_keys) { ["otherAttribute","@anotherAttribute","Array","Object"] }
 
+        subject { Key.for :attribute_name, given: hash_keys }
+
+        specify do
+          hash_keys.concat ["AttributeName", "attributeName", "@attributeName"]
+          subject.should == "attributeName"
+        end
+
+        specify do
+          hash_keys.concat ["AttributeName", "@attributeName"]
+          subject.should == "@attributeName"
+        end
+
+        specify do
+          hash_keys.concat ["AttributeName"]
+          subject.should == "AttributeName"
+        end
+      end
+
+      describe "lower_camel_case(name,keys)" do
+        let(:keys) { [] }
+
+        subject { Key.lower_camel_case :attribute_name, keys }
+
+        it "should return lower camelcase named when keys contain name" do
+          keys << "attributeName"
+          subject.should == "attributeName"
+        end
+        it "should return nil when keys do not contain name" do
+          subject.should be_nil
+        end
+
+        context "keys are ommited" do
+          it "should return lower camelcase name" do
+            Key.lower_camel_case(:attribute_name).should == "attributeName"
+          end
+        end
+      end
+
+      describe "upper_camel_case(name,keys)" do
+        let(:keys) { [] }
+
+        subject { Key.upper_camel_case :attribute_name, keys }
+
+        it "should return upper camelcase named when keys contain name" do
+          keys << "AttributeName"
+          subject.should == "AttributeName"
+        end
+        it "should return nil when keys do not contain name" do
+          subject.should be_nil
+        end
+
+        context "keys are ommited" do
+          it "should return upper camelcase name" do
+            Key.upper_camel_case(:attribute_name).should == "AttributeName"
+          end
+        end
+      end
+
+      describe "at_symbol_case(name,keys)" do
+        let(:keys) { [] }
+
+        subject { Key.at_symbol_case :attribute_name, keys }
+
+        it "should return symbol case named when keys contain name" do
+          keys << "@attributeName"
+          subject.should == "@attributeName"
+        end
+        it "should return nil when keys do not contain name" do
+          subject.should be_nil
+        end
+
+        context "keys are ommited" do
+          it "should return at symbol case" do
+            Key.at_symbol_case(:attribute_name).should == "@attributeName"
+          end
+        end
+      end
     end
   end
 end
