bd4c727db913ada537f444ea4bd9d5848ec98207
Jon Rowe <mail@jonrowe.co.uk>
2011-05-19 15:51:54 +0100

    bool value setting

--- lib/setsumei/describable/boolean_attribute.rb
+++ lib/setsumei/describable/boolean_attribute.rb
@@ -17,6 +17,18 @@ module Setsumei
       def value_for(pre_type_cast_value)
         pre_type_cast_value.to_s.downcase == "true" || pre_type_cast_value.to_s == "1"
       end
+
+      def set_value_on(object, options)
+        object.send accessor, value_from_hash(options[:from_value_in])
+      end
+
+      private
+        def accessor
+          :"#{name}="
+        end
+        def value_from_hash(hash)
+          value_for hash[ Build::Key.for name, given: hash.keys ]
+        end
     end
   end
 end
