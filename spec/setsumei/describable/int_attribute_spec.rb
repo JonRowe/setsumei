cc8a0e4867713465d4fb2e2427b2355ca95db796
Jon Rowe <mail@jonrowe.co.uk>
2011-05-19 15:45:46 +0100

    int value setting

--- spec/setsumei/describable/int_attribute_spec.rb
+++ spec/setsumei/describable/int_attribute_spec.rb
@@ -48,6 +48,40 @@ module Setsumei
           it { should be_false }
         end
       end
+
+      describe "set_value_on object, from_value_in: hash" do
+        let(:hash) { Hash.new }
+        let(:key) { "key" }
+        let(:hash_keys) { mock "hash_keys" }
+        let(:value_in_hash) { mock "value_in_hash" }
+
+        let(:object) { mock "object", :my_int_attribute= => nil }
+
+        let(:int_attribute) { IntAttribute.named :my_int_attribute }
+        let(:converted_value) { mock "converted_value" }
+
+        before do
+          Build::Key.stub(:for).and_return(key)
+          hash[key] = value_in_hash
+          int_attribute.stub(:value_for).and_return(converted_value)
+        end
+
+        subject { int_attribute.set_value_on object, from_value_in: hash }
+
+        it "should detect the key it should use to retrieve the value from the hash" do
+          hash.should_receive(:keys).and_return(hash_keys)
+          Build::Key.should_receive(:for).with(:my_int_attribute, given: hash_keys ).and_return(key)
+          subject
+        end
+        it "should convert the value" do
+          int_attribute.should_receive(:value_for).with(value_in_hash).and_return(converted_value)
+          subject
+        end
+        it "should pass object a value to the attribute described by this class" do
+          object.should_receive(:my_int_attribute=).with(converted_value)
+          subject
+        end
+      end
     end
   end
 end
